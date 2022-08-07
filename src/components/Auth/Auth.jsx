28044117b22fae39cd50db99e167a8de15b3e24d
Matthew4T <matvey.tishin@yandex.ru>
2022-08-07 22:53:02 +0300

    code review

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -3,18 +3,7 @@ import Header from "../Header/Header";
 import styles from "./Auth.module.scss"
 import {domain, thisDomain, clientId} from "../baseComponents/baseFunctions"
 function Redirect() {
-    const raspMas = [
-        {"course_name": "Русский язык", "webinars_block": [
-                {"month": "Сентябрь", "webinars": ["№4 Орфоэпия", "№5 Паронимы", "Сочинение. Поиск проблемы", "№6 Лексика"]},
-                {"month": "Октябрь", "webinars": ["№7 Числительные", "Практика по сложным темам №4-7", "Сочинение. Смысловая связь между комментариями", "№8 Синтаксические нормы"]}
-            ]
-        },
-        {"course_name": "Физика", "webinars_block": [
-                {"month": "Сентябрь", "webinars": ["Основы кинематики. Виды движения", "Основы кинематики. Виды движения. Практика А части", "Движение по параболе"]},
-                {"month": "Октябрь", "webinars": ["Импульс. Закон сохранения импульса. Практика С части", "Энергия. Закон сохранения энергии", "Энергия. Закон сохранения энергии. Практика А части", "Энергия. Закон сохранения энергии. Практика С части"]}
-            ]
-        }
-    ]
+
 
     //let redirectUri = "https://lk.lyc15.ru/ant"
     let redirectUri = `${thisDomain}/ant`
